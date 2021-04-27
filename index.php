4a61c3689a7d809b13bc9059f35d65a76fdf08fd
Kamil Kowalczyk <kkowalczyk@ifxpayments.com>
2021-04-27 18:11:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'vendor/autoload.php';
+
+$faker = Faker\Factory::create('pl_PL');
+
+echo "\e[91m ATTENTION!!!\e[39m In order to delete entries in csv file enter number 0 - 'EXAMPLE: php index.php 0'. " . PHP_EOL;
+sleep(7);
+
+$numberOfEntries = abs((int) $argv[1]);
+
+if ($numberOfEntries == 0) {
+    echo "\e[91m You just deleted all entries from the csv file. Well done!\e[32m " . PHP_EOL;
+} elseif ($numberOfEntries !== 0 && $numberOfEntries < 1001) {
+    echo "\e[32m CONGRATULATIONS! You just generated and saved \e[93m$numberOfEntries\e[32m entries to csv file. " . PHP_EOL;
+    exit;
+} else {
+    echo "\e[91m ERROR!\e[39m You have to enter a number less than 1000 and greater than 0. " . PHP_EOL;
+}
+
+$fakeData [] = [
+    'title',
+    'first_name',
+    'last_name',
+    'age',
+    'email',
+    'phone_number',
+    'city',
+    'post_code',
+    'street_name',
+    'building_number'
+];
+
+for ($i = 0; $i < $numberOfEntries; $i++) {
+    $fakeData [] = [
+        $faker->title,
+        $faker->firstName,
+        $faker->lastName,
+        $faker->numberBetween($min = 18, $max = 80),
+        $faker->email,
+        $faker->phoneNumber,
+        $faker->city,
+        $faker->postcode,
+        $faker->streetName,
+        $faker->buildingNumber,
+    ];
+ }
+
+$openCsvFile = fopen('file.csv', 'w');
+
+foreach ($fakeData as $row) {
+    fputcsv($openCsvFile, $row);
+}
+
+fclose($openCsvFile);
