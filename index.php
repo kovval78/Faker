a0d35371b9ae2c45e0789bbb7f7ed9202add0593
Kamil Kowalczyk <kkowalczyk@ifxpayments.com>
2021-04-28 11:58:59 +0200

    Correct error in the index.php file

--- index.php
+++ index.php
@@ -2,22 +2,24 @@
 
 declare(strict_types=1);
 
+use Console\Console;
+
 require_once 'vendor/autoload.php';
 
 $faker = Faker\Factory::create('pl_PL');
 
 echo "\e[91m ATTENTION!!!\e[39m In order to delete entries in csv file enter number 0 - 'EXAMPLE: php index.php 0'. " . PHP_EOL;
-sleep(7);
+sleep(1);
 
 $numberOfEntries = abs((int) $argv[1]);
 
-if ($numberOfEntries == 0) {
+if ($numberOfEntries === 0) {
     echo "\e[91m You just deleted all entries from the csv file. Well done!\e[32m " . PHP_EOL;
-} elseif ($numberOfEntries !== 0 && $numberOfEntries < 1001) {
-    echo "\e[32m CONGRATULATIONS! You just generated and saved \e[93m$numberOfEntries\e[32m entries to csv file. " . PHP_EOL;
-    exit;
-} else {
+} elseif ($numberOfEntries >= 1001)  {
     echo "\e[91m ERROR!\e[39m You have to enter a number less than 1000 and greater than 0. " . PHP_EOL;
+    return;
+} else {
+    echo "\e[32m CONGRATULATIONS! You just generated and saved \e[93m$numberOfEntries\e[32m entries to csv file. " . PHP_EOL;
 }
 
 $fakeData [] = [
